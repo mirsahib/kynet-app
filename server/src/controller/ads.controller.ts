ec469c8ee0f14cedb1a06de5eb9fc1e4e20da0ae
Mir Sahib <mirsahib24@gmail.com>
2023-03-26 21:15:50 +0600

    :sparkles: Add ads crud

---
+++ server/src/controller/ads.controller.ts
@@ -0,0 +1,99 @@
+import { NextFunction, Request, Response } from "express";
+import {
+	createAds,
+	findOneAds,
+    findAllAds,
+	IAds,
+} from "../database/repository/Ads.repository";
+import handleError from "../util/errorHandles";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+	cloud_name: "dfdmsw4lg",
+	api_key: "117463879357159",
+	api_secret: "VHN7mjO5KDXvFiI07Tr-zqF2Ijc",
+});
+
+interface RequestParams {
+	adsId: string;
+}
+
+const create = async (req: Request, res: Response) => {
+	try {
+		const ads = await createAds(req.body);
+		res.status(200).json({ message: "image upload successful", data: ads });
+	} catch (error) {
+		return res.status(500).json(handleError(error));
+	}
+};
+const read= async (req: Request<RequestParams>, res: Response) => {
+    return res.json({"ads":req.body.data})
+}
+
+const list = async (req: Request, res: Response) => {
+    try {
+        const allAds = await findAllAds()
+        return res.status(200).json({ads:allAds})
+    } catch (error) {
+        return res.status(500).json(handleError(error));
+    }
+}
+
+const findAdsbyId = async (req: Request<RequestParams>, res: Response,next:NextFunction) => {
+	try {
+        console.log('params',req.params.adsId)
+		const ads = await findOneAds(req.params.adsId);
+        if(!ads){
+            return res.status(401).json({
+				error: "ads not found",
+			});
+        }
+        req.body.data = ads
+        next()
+        //res.status(200).json({ads:ads});
+	} catch (error) {
+        return res.status(500).json(handleError(error));
+    }
+};
+
+
+const uploadImage = async (
+	req: Request<{}, {}, IAds>,
+	res: Response,
+	next: NextFunction
+) => {
+	if (req.file) {
+		try {
+			const buf = req.file.buffer.toString("base64");
+			const res = await cloudinary.uploader.upload(
+				"data:image/png;base64," + buf,
+				{
+					upload_preset: "kynet",
+					folder: "kynet",
+					public_id: `${Date.now()}`,
+					resource_type: "auto",
+				}
+			);
+			req.body.image = res.url;
+			next();
+		} catch (error) {
+			console.log("upload error", error);
+			return res.status(500).json(handleError(error));
+		}
+	} else {
+		return res.status(400).json({
+			error: {
+				name: "ValidationError",
+				message: "image is required",
+			},
+		});
+	}
+};
+
+export default {
+	create,
+    read,
+    list,
+	findAdsbyId,
+	uploadImage,
+};
