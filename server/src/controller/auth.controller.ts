41c2ccb6d04ff2a5504f8b28cc2af215b4585c84
Mir Sahib <mirsahib24@gmail.com>
2023-04-07 15:10:32 +0600

    :construction: working on proxy setup

--- server/src/controller/auth.controller.ts
+++ server/src/controller/auth.controller.ts
@@ -37,7 +37,7 @@ const login = async (req: Request<{}, {}, IUser>, res: Response) => {
 		res.cookie("token", token, {
 			expires: expiryDate,
 			httpOnly: true,
-			secure:false,
+			secure:true,
 			sameSite:'none'
 		});
 
