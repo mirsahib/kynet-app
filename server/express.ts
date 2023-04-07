3f41f4d6452116d525fa7acc5253e3769f5849e4
Mir Sahib <mirsahib24@gmail.com>
2023-04-07 16:43:41 +0600

    :wrench: Add cors configuraition

--- server/express.ts
+++ server/express.ts
@@ -1,15 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import cors from 'cors'
+import cors, { CorsOptions } from 'cors'
 import cookieParser from 'cookie-parser';
 import userRoutes from "./src/routes/user.routes"
 import authRoutes from './src/routes/auth.routes'
 import adsRoutes from './src/routes/ads.routes'
 const app = express();
 
+const corsOptions:CorsOptions = {
+    origin: true,
+    credentials:true,
+}
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(cookieParser())
 
 app.use("/",userRoutes)
