b0d1b1810e44f46a967875da7d0ce0bd8b180c99
Mir Sahib <mirsahib24@gmail.com>
2023-04-07 11:59:20 +0600

    :wrench: Add proxy setup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -18,6 +18,14 @@ export default defineConfig({
   },
   server:{
     host:true,
-    port:3001
-  }
+    port:3001,
+    proxy:{
+      '/api':{
+        target:'http://172.19.0.4:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+  },
+  
 })
