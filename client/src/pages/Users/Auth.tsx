86f44a3a5843d4dbbb1097ef437604e10b6567f9
Mir Sahib <mirsahib24@gmail.com>
2023-04-11 13:23:26 +0600

    :label: Add style types

--- client/src/pages/Users/Auth.tsx
+++ client/src/pages/Users/Auth.tsx
@@ -1,29 +1,45 @@
-import { Link, Outlet, Route, Routes } from "react-router-dom";
+import { NavLink, NavLinkProps, Outlet, useMatch } from "react-router-dom";
 
 export default function Auth() {
+	const location = useMatch('/form/')
+
 	return (
 		<div className="flex justify-center  mx-auto ">
 			<div className="flex flex-col lg:w-[520px] sm:w-[5/6] bg-green-600 ">
 				<ul className="flex flex-row bg-green-500">
-					<li className="flex w-[50%] bg-green-600">
-						<Link
-							to="signup"
-							className="flex w-full justify-center py-4 text-white font-semibold"
+					<li className={`flex w-[50%] ${location?.pathname==='/form'?"bg-green-600":""}`} >
+						<NavLink
+							to={"signup"}
+							className={({ isActive,isPending }) =>
+								isPending ? `${style.inActive}`:
+								isActive
+									? `${style.active}`
+									: `${style.inActive}`
+							}
 						>
 							Sign Up
-						</Link>
+						</NavLink>
 					</li>
 					<li className="flex w-[50%] ">
-						<Link
+						<NavLink
 							to="login"
-							className="flex  w-full justify-center py-4 text-white font-semibold"
+							className={({ isActive }) =>
+								isActive
+									? `${style.active}`
+									: `${style.inActive}`
+							}
 						>
 							Log In
-						</Link>
+						</NavLink>
 					</li>
 				</ul>
-				<Outlet/>
+				<Outlet />
 			</div>
 		</div>
 	);
 }
+
+const style:{[key:string]:NavLinkProps["className"]} = {
+	active: "flex w-full justify-center py-4 text-white font-semibold bg-green-600",
+	inActive: "flex w-full justify-center py-4 text-white font-semibold",
+};
