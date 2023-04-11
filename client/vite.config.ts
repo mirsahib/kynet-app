7d8bcb1b45a177264b157256b243a4d71a6a7b40
Mir Sahib <mirsahib24@gmail.com>
2023-04-11 21:57:17 +0600

    :bug: Update proxy link

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -21,14 +21,14 @@ export default defineConfig({
     port:3001,
     proxy:{
       '/api':{
-        target:'http://kynet_server:3000',
+        target:'https://kynet-server-production.up.railway.app/',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
+        rewrite: (path) => path.replace(/^\/api/, 'api'),
       },
       '/auth':{
-        target:'http://kynet_server:3000',
+        target:'https://kynet-server-production.up.railway.app/',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/auth'),
+        rewrite: (path) => path.replace(/^\/api/, 'auth'),
       }
     }
   },
